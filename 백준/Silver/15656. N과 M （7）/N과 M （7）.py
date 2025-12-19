9af9411245f248f9b435d397d93d1cb4259a1abe
박하민 <hamin090403@gmail.com>
2025-12-19 10:42:38 +0900

    [Silver III] Title: N과 M (7), Time: 2072 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+bt = []
+lst = list(map(int, input().split()))
+sort_list = sorted(lst)
+visited = [False] * n
+
+def nm(depth):
+    if depth == m:
+        print(*bt)
+        return
+    for i in range(n):
+
+        bt.append(sort_list[i])
+        visited[i] = True
+
+        nm(depth + 1)
+
+        bt.pop()
+        visited[i] = False    
+
+nm(0)
