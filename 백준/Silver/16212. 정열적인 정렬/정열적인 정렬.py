f0611b52e9efeccfad285f437f97a91bfb93c532
박하민 <hamin090403@gmail.com>
2025-12-22 00:09:48 +0900

    [Silver V] Title: 정열적인 정렬, Time: 660 ms, Memory: 86596 KB, Score: 25 point -BaekjoonHub

---
+++ 백준/Silver/16212. 정열적인 정렬/정열적인 정렬.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+num = map(int, input().split())
+
+set_list = sorted(list(num))
+for i in range(len(set_list)):
+    print(set_list[i], end = " ")
